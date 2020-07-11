7d35dddd5ccd809d98d622aa663363736f1037f0
duyanhnguyenxuan <duyanh1202tm@gmail.com>
2020-07-11 21:00:08 +0700

    second commit

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
-var i = 1;
-async function getMultiplePage() {
-    while (i <= 10) {
+
+for (var i = 1; i <= 10; i++) {
+    var url = 'https://dantri.com.vn/xa-hoi.htm'
+
+    if (i == 1) {
         var url = 'https://dantri.com.vn/xa-hoi.htm'
 
-        if (i == 1) {
-            console.log(url)
-        } else {
-            url = url.replace(/.htm/, '/trang-' + i + '.htm')
-            console.log(url)
-        }
-
-        await request(url, (err, res, body) => {
-            if (!err && res.statusCode == 200) {
-                const $ = cheerio.load(body);
-                $('.mt3').each((index, item) => {
-                    const title = $(item).find('.mr1 h2 a').text();
-                    console.log(`Title trang ${i} :  ${title}`)
-                })
-            }
-        })
-
-        i++;
+        console.log(url)
+    } else {
+        url = url.replace(/.htm/, '/trang-' + i + '.htm')
+        console.log(url)
     }
+
+    request(url, (err, res, body) => {
+        if (!err && res.statusCode == 200) {
+            const $ = cheerio.load(body);
+            $('.mt3').each((index, item) => {
+                const title = $(item).find('.mr1 h2 a').text();
+                console.log(`Title trang ${i} :  ${title}`)
+            })
+        }
+    })
+
 }
